822416119387470162ba9d7541f7a3a61085be30
AndreasDEV <andreasmrdja16@gmail.com>
2019-06-14 22:15:24 +0200

    Setup development environment. Initial Commit.

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1>Title</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  font-size: 6rem;
+}
+</style>
